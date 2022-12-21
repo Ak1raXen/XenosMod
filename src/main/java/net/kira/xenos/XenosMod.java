7151338a154a9d623bc89c546413824468b37f56
Akira Koyama <akiramkoyama@gmail.com>
2022-12-21 19:50:26 +0700

    Initial commit

---
+++ src/main/java/net/kira/xenos/XenosMod.java
@@ -0,0 +1,17 @@
+package net.kira.xenos;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XenosMod implements ModInitializer {
+	public static final String MOD_ID = "xenos";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
+
+
---
+++ src/main/java/net/kira/xenos/XenosModClient.java
@@ -0,0 +1,10 @@
+package net.kira.xenos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XenosModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
